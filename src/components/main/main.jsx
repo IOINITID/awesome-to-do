f97f0344f9038354654b17bf203831b65bbb8754
IOINITID <50173108+IOINITID@users.noreply.github.com>
2020-09-22 19:24:56 +0300

    Updated index.html styles

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
-import './main.css';
+// import './main.css';
 
 export default class Main extends Component {
   // constructor(props) {
