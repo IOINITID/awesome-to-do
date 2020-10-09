2f9f5becd7a0627c6a7a3285efeb2c04f5a7daa9
IOINITID <50173108+IOINITID@users.noreply.github.com>
2020-10-09 22:45:29 +0300

    Added provider and store

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import store from './store/index.js';
+import {Provider} from 'react-redux';
 import App from './components/app/app.jsx';
 
-ReactDOM.render(<App />, document.querySelector(`.root`));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.querySelector(`.root`));
