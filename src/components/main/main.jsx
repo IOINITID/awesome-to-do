51c04ca00f711118159de9b1ca7cf4562f3c8c50
IOINITID <50173108+IOINITID@users.noreply.github.com>
2020-10-10 15:18:27 +0300

    Switched main from class component to function component

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,59 +1,53 @@
-import React, {Component} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Menu from '../menu/menu.jsx';
 import Tasks from '../tasks/tasks.jsx';
 import Greeting from '../greeting/greeting.jsx';
 import Info from '../info/info.jsx';
 
-export default class Main extends Component {
-  constructor() {
-    super();
-  }
+const Main = (props) => {
+  const {itemsData, searching, wellcomeDefault, filterType, onModalSwitch, onDoneSwitch, onTaskFixed, onFilterChange, itemsQuantity} = props;
 
-  render() {
-    const {itemsData, searching, wellcomeDefault, filterType, menuDefault, onModalSwitch, onDoneSwitch, onTaskFixed, onFilterChange, onMenuSwitch, itemsQuantity} = this.props;
+  const getNoTasksComponent = () => {
+    switch (wellcomeDefault) {
+      case `true`:
+        return <Greeting />;
+      case `false`:
+        return <Info filterType={filterType} searching={searching} />;
+      default:
+        return <Greeting />;
+    }
+  };
 
-    const getNoTasksComponent = () => {
-      switch (wellcomeDefault) {
-        case `true`:
-          return <Greeting />;
-        case `false`:
-          return <Info filterType={filterType} searching={searching} />;
-        default:
-          return <Greeting />;
-      }
-    };
+  return (
+    <main className="main">
 
-    return (
-      <main className="main">
+      <div className="container">
+        <Menu itemsQuantity={itemsQuantity} onModalSwitch={onModalSwitch} onFilterChange={onFilterChange} />
+      </div>
 
-        <div className="container">
-          <Menu itemsQuantity={itemsQuantity} onModalSwitch={onModalSwitch} menuDefault={menuDefault} onFilterChange={onFilterChange} onMenuSwitch={onMenuSwitch}></Menu>
+      {
+        itemsData.length ? <div className="container">
+          <Tasks itemsData={itemsData} filterType={filterType} onDoneSwitch={onDoneSwitch} onTaskFixed={onTaskFixed} onModalSwitch={onModalSwitch} />
+        </div> : <div className="container">
+          {getNoTasksComponent()}
         </div>
+      }
 
-        {
-          itemsData.length ? <div className="container">
-            <Tasks itemsData={itemsData} filterType={filterType} onDoneSwitch={onDoneSwitch} onTaskFixed={onTaskFixed} onModalSwitch={onModalSwitch}></Tasks>
-          </div> : <div className="container">
-            {getNoTasksComponent()}
-          </div>
-        }
-
-      </main>
-    );
-  }
-}
+    </main>
+  );
+};
 
 Main.propTypes = {
   itemsData: PropTypes.array.isRequired,
-  menuDefault: PropTypes.bool.isRequired,
   onModalSwitch: PropTypes.func.isRequired,
   onDoneSwitch: PropTypes.func.isRequired,
   onTaskFixed: PropTypes.func.isRequired,
   onFilterChange: PropTypes.func.isRequired,
-  onMenuSwitch: PropTypes.func.isRequired,
   itemsQuantity: PropTypes.array.isRequired,
   filterType: PropTypes.string.isRequired,
   wellcomeDefault: PropTypes.string.isRequired,
   searching: PropTypes.bool.isRequired
 };
+
+export default Main;
