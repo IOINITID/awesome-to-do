9e1f970b52ad75596fddcfa59becdc83de441d22
IOINITID <50173108+IOINITID@users.noreply.github.com>
2020-10-10 15:02:12 +0300

    Switched header from class component to function component

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,34 +1,30 @@
-import React, {Component} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Navigation from '../navigation/navigation.jsx';
 import Search from '../search/search.jsx';
 import Switch from '../switch/switch.jsx';
 import Logo from '../logo/logo.jsx';
 
-export default class Header extends Component {
-  constructor() {
-    super();
-  }
+const Header = (props) => {
 
-  render() {
-    const {onSearchChange, onMenuSwitch, onModalSwitch, onSearching} = this.props;
+  const {onSearchChange, onModalSwitch, onSearching} = props;
 
-    return (
-      <header className="header">
-        <div className="container container--flex">
-          <Navigation onMenuSwitch={onMenuSwitch} onModalSwitch={onModalSwitch}></Navigation>
-          <Search onSearching={onSearching} onSearchChange={onSearchChange}></Search>
-          <Switch></Switch>
-          <Logo></Logo>
-        </div>
-      </header>
-    );
-  }
-}
+  return (
+    <header className="header">
+      <div className="container container--flex">
+        <Navigation onModalSwitch={onModalSwitch}></Navigation>
+        <Search onSearching={onSearching} onSearchChange={onSearchChange}></Search>
+        <Switch></Switch>
+        <Logo></Logo>
+      </div>
+    </header>
+  );
+};
 
 Header.propTypes = {
   onSearchChange: PropTypes.func.isRequired,
-  onMenuSwitch: PropTypes.func.isRequired,
   onModalSwitch: PropTypes.func.isRequired,
   onSearching: PropTypes.func.isRequired
 };
+
+export default Header;
