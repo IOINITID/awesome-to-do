5636426788ab07ca91e5058c942e1905c218a8f1
IOINITID <50173108+IOINITID@users.noreply.github.com>
2020-10-11 23:05:10 +0300

    Removed unused properties

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,27 +1,20 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import Navigation from '../navigation/navigation.jsx';
 import Search from '../search/search.jsx';
 import Switch from '../switch/switch.jsx';
 import Logo from '../logo/logo.jsx';
 
-const Header = (props) => {
-  const {onModalSwitch} = props;
-
+const Header = () => {
   return (
     <header className="header">
       <div className="container container--flex">
-        <Navigation onModalSwitch={onModalSwitch} />
+        <Navigation />
         <Search />
         <Switch />
         <Logo />
       </div>
     </header>
   );
-};
-
-Header.propTypes = {
-  onModalSwitch: PropTypes.func.isRequired
 };
 
 export default Header;
