a7e20b95f71211511613360d8408dcb5f9a29a16
IOINITID <50173108+IOINITID@users.noreply.github.com>
2020-10-11 23:11:35 +0300

    Updated tasks component to Redux

--- src/components/tasks/tasks.jsx
+++ src/components/tasks/tasks.jsx
@@ -1,74 +1,75 @@
-import React, {Component} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Task from '../task/task.jsx';
+import {connect} from 'react-redux';
 
-export default class Tasks extends Component {
-  constructor() {
-    super();
+const Tasks = (props) => {
+  const {itemsData, filterType, onModalSwitch} = props;
+
+  let tasksTitle;
+
+  switch (filterType) {
+    case `all`:
+      tasksTitle = `Все задачи`;
+      break;
+    case `done`:
+      tasksTitle = `Выполненные`;
+      break;
+    case `undone`:
+      tasksTitle = `Текущие`;
+      break;
+    case `fixed`:
+      tasksTitle = `Закреплённые`;
+      break;
+    default:
+      tasksTitle = `Все задачи`;
+      break;
   }
 
-  render() {
-    const {itemsData, filterType, onDoneSwitch, onTaskFixed, onModalSwitch} = this.props;
+  return (
+    <section className="tasks">
+      <h2 className="tasks__title">{tasksTitle}</h2>
+      <ul className="tasks__list">
+        {
+          itemsData.map((item) => {
+            const {id, title, done, fixed} = item;
 
-    let tasksTitle;
+            let tasksItemClassName;
 
-    switch (filterType) {
-      case `all`:
-        tasksTitle = `Все задачи`;
-        break;
-      case `done`:
-        tasksTitle = `Выполненные`;
-        break;
-      case `undone`:
-        tasksTitle = `Текущие`;
-        break;
-      case `fixed`:
-        tasksTitle = `Закреплённые`;
-        break;
-      default:
-        tasksTitle = `Все задачи`;
-        break;
-    }
+            switch (true) {
+              case fixed:
+                tasksItemClassName = `tasks__item tasks__item--fixed`;
+                break;
+              case done:
+                tasksItemClassName = `tasks__item tasks__item--done`;
+                break;
+              default:
+                tasksItemClassName = `tasks__item`;
+                break;
+            }
 
-    return (
-      <section className="tasks">
-        <h2 className="tasks__title">{tasksTitle}</h2>
-        <ul className="tasks__list">
-          {
-            itemsData.map((item) => {
-              const {id, title, done, fixed} = item;
-
-              let tasksItemClassName;
-
-              switch (true) {
-                case fixed:
-                  tasksItemClassName = `tasks__item tasks__item--fixed`;
-                  break;
-                case done:
-                  tasksItemClassName = `tasks__item tasks__item--done`;
-                  break;
-                default:
-                  tasksItemClassName = `tasks__item`;
-                  break;
-              }
-
-              return (
-                <li key={id} className={tasksItemClassName}>
-                  <Task id={id} title={title} done={done} fixed={fixed} onDoneSwitch={onDoneSwitch} onTaskFixed={onTaskFixed} onModalSwitch={onModalSwitch} />
-                </li>
-              );
-            })
-          }
-        </ul>
-      </section>
-    );
-  }
-}
+            return (
+              <li key={id} className={tasksItemClassName}>
+                <Task id={id} title={title} done={done} fixed={fixed} onModalSwitch={onModalSwitch} />
+              </li>
+            );
+          })
+        }
+      </ul>
+    </section>
+  );
+};
 
 Tasks.propTypes = {
   itemsData: PropTypes.array.isRequired,
-  onDoneSwitch: PropTypes.func.isRequired,
-  onTaskFixed: PropTypes.func.isRequired,
   onModalSwitch: PropTypes.func.isRequired,
   filterType: PropTypes.string.isRequired
 };
+
+const mapStateToProps = (state) => {
+  return {
+    filterType: state.filterType
+  };
+};
+
+export default connect(mapStateToProps)(Tasks);
