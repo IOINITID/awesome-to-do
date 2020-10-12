0568e724f0bb586ae455d5a4efbd0522de4cf371
IOINITID <50173108+IOINITID@users.noreply.github.com>
2020-10-12 11:55:42 +0300

    Updated greeting component

--- src/components/greeting/greeting.jsx
+++ src/components/greeting/greeting.jsx
@@ -1,23 +1,16 @@
-import React, {Component} from 'react';
-// import PropTypes from 'prop-types';
+import React from 'react';
 
-export default class Greeting extends Component {
-  constructor() {
-    super();
-  }
+const Greeting = () => {
+  return (
+    <section className="greeting">
+      <div className="greeting__info">
+        <h2 className="greeting__title">Awesome to do</h2>
+        <p className="greeting__description">Ваш личный помощник<br /> в организации списка задач</p>
+      </div>
+      <img className="greeting__image greeting__image--dark" src="./img/greeting-dark-icon.svg" alt="Человек заполняет список дел." />
+      <img className="greeting__image greeting__image--light" src="./img/greeting-light-icon.svg" alt="Человек заполняет список дел." />
+    </section>
+  );
+};
 
-  render() {
-    return (
-      <section className="greeting">
-        <div className="greeting__info">
-          <h2 className="greeting__title">Awesome to do</h2>
-          <p className="greeting__description">Ваш личный помощник<br /> в организации списка задач</p>
-        </div>
-        <img className="greeting__image greeting__image--dark" src="./img/greeting-dark-icon.svg" alt="Человек заполняет список дел." />
-        <img className="greeting__image greeting__image--light" src="./img/greeting-light-icon.svg" alt="Человек заполняет список дел." />
-      </section>
-    );
-  }
-}
-
-// Greeting.propTypes = {};
+export default Greeting;
