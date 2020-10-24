2c5aed72b43651b5b009c7d06efb8796f21fcb73
IOINITID <50173108+IOINITID@users.noreply.github.com>
2020-10-24 11:12:55 +0300

    Made code refactoring

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, {useEffect} from 'react';
 import PropTypes from 'prop-types';
 import Header from '../header/header.jsx';
 import Main from '../main/main.jsx';
