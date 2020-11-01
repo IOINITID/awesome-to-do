c0ed311fd71a8c2a81796927439178af38c81638
IOINITID <50173108+IOINITID@users.noreply.github.com>
2020-11-01 12:17:05 +0300

    Revert "Updated header, logo, navigation and menu components"
    This reverts commit 10e5f90b53ae8aa156ec2e8251943bec3d05ad15.

--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {onMenuSwitchAction, onFilterChangeAction, onSearchSwitchAction, onSearchChangeAction} from '../../actions/index.js';
-import {v4 as uuid} from 'uuid';
-import Icon from '../icon/icon.jsx';
 
 const Menu = (props) => {
   const {itemsQuantity, menuDefault, onModalSwitch, onMenuSwitch, onFilterChange, onSearchSwitch, onSearchChange} = props;
@@ -31,85 +29,43 @@ const Menu = (props) => {
 
   const menuClassName = menuDefault ? `menu` : `menu menu--active`;
 
-  const menuItemsData = [
-    {
-      id: uuid(),
-      title: `Все задачи`,
-      dataType: `all`,
-      events: {
-        onClick: onFilterItemClick
-      },
-      quantity: itemsAll,
-      iconData: null
-    },
-    {
-      id: uuid(),
-      title: `Текущие`,
-      dataType: `undone`,
-      events: {
-        onClick: onFilterItemClick
-      },
-      quantity: itemsNotDone,
-      iconData: null
-    },
-    {
-      id: uuid(),
-      title: `Выполненные`,
-      dataType: `done`,
-      events: {
-        onClick: onFilterItemClick
-      },
-      quantity: itemsDone,
-      iconData: null
-    },
-    {
-      id: uuid(),
-      title: `Закреплённые`,
-      dataType: `fixed`,
-      events: {
-        onClick: onFilterItemClick
-      },
-      quantity: null,
-      iconData: {
-        className: `menu`,
-        name: `pin`,
-        width: 17,
-        height: 17
-      }
-    },
-    {
-      id: uuid(),
-      title: `Добавить задачу`,
-      dataType: `add`,
-      events: {
-        onClick: onAddLinkClick
-      },
-      quantity: null,
-      iconData: {
-        className: `menu`,
-        name: `add`,
-        width: 17,
-        height: 17
-      }
-    }
-  ];
-
-  const menuItems = menuItemsData.map((item) => {
-    return (
-      <li key={item.id} className={`menu__item menu__item--${item.dataType}`}>
-        <a className="menu__link" href="#" data-type={item.dataType} onClick={item.events.onClick}>
-          {item.iconData && <Icon iconData={item.iconData} />}
-          {item.title && item.title}
-          {item.quantity !== null && <sup className="menu__quantity">{item.quantity}</sup>}
-        </a>
-      </li>
-    );
-  });
-
   return (
     <section className={menuClassName}>
       <ul className="menu__list">
-        {menuItems}
+        <li className="menu__item">
+          <a className="menu__link" href="#" data-type="all" onClick={onFilterItemClick}>Все задачи<sup className="menu__quantity">{itemsAll}</sup></a>
+        </li>
+        <li className="menu__item">
+          <a className="menu__link" href="#" data-type="undone" onClick={onFilterItemClick}>Текущие<sup className="menu__quantity">{itemsNotDone}</sup></a>
+        </li>
+        <li className="menu__item">
+          <a className="menu__link" href="#" data-type="done" onClick={onFilterItemClick}>Выполненные<sup className="menu__quantity">{itemsDone}</sup></a>
+        </li>
+        <li className="menu__item menu__item--fixed">
+          <a className="menu__link" href="#" data-type="fixed" onClick={onFilterItemClick}>
+            <svg className="menu__icon" width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <g clipPath="url(#clip0)">
+                <path
+                  d="M13.0971 12.1102L16.7924 8.41098C16.9616 8.24312 17.033 8.0009 16.9856 7.76718C16.9375 7.53415 16.7755 7.3401 16.554 7.25229C14.0468 6.25152 12.1481 6.46825 11.3435 6.64178L6.87892 2.83985C6.96738 1.33552 6.15923 0.310689 6.12172 0.263944C5.99646 0.107409 5.80964 0.0117947 5.60866 0.000473745C5.40979 -0.00659773 5.21166 0.0656442 5.07012 0.207273L0.207182 5.07441C0.0642475 5.21676 -0.0107859 5.41437 0.00125525 5.61622C0.0125666 5.81738 0.109526 6.00505 0.268052 6.12971C1.13704 6.82096 2.2233 6.89393 2.80005 6.87906L6.63269 11.3878C6.49327 12.1938 6.31355 14.1705 7.26178 16.5538C7.34952 16.7755 7.54271 16.937 7.77554 16.9858C7.82367 16.995 7.87107 17 7.91847 17C8.10459 17 8.28505 16.9271 8.41879 16.7925L12.0965 13.1117L15.7734 16.7925C15.9114 16.9306 16.0926 17 16.2737 17C16.4549 17 16.6361 16.9306 16.7741 16.7925C17.0507 16.5156 17.0507 16.0679 16.7741 15.791L13.0971 12.1102Z"
+                  fill="#F9E4B0" />
+              </g>
+              <defs>
+                <clipPath id="clip0">
+                  <rect width="17" height="17" fill="white" />
+                </clipPath>
+              </defs>
+            </svg>
+                  Закреплённые</a>
+        </li>
+        <li className="menu__item menu__item--add">
+          <a className="menu__link" href="#" onClick={onAddLinkClick}>
+            <svg className="menu__icon" width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M16.3359 7.83594H9.16406V0.664062C9.16406 0.297301 8.86676 0 8.5 0C8.13324 0 7.83594 0.297301 7.83594 0.664062V7.83594H0.664062C0.297301 7.83594 0 8.13324 0 8.5C0 8.86676 0.297301 9.16406 0.664062 9.16406H7.83594V16.3359C7.83594 16.7027 8.13324 17 8.5 17C8.86676 17 9.16406 16.7027 9.16406 16.3359V9.16406H16.3359C16.7027 9.16406 17 8.86676 17 8.5C17 8.13324 16.7027 7.83594 16.3359 7.83594Z"
+                fill="#85DC9D" />
+            </svg>
+                  Добавить задачу</a>
+        </li>
       </ul>
     </section>
   );
--- src/components/navigation/navigation.jsx
+++ src/components/navigation/navigation.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {v4 as uuid} from 'uuid';
 import {connect} from 'react-redux';
 import {onMenuSwitchAction, onModalSwitchAction, onSearchSwitchAction, onSearchChangeAction} from '../../actions/index.js';
-import Button from '../button/button.jsx';
-import Icon from '../icon/icon.jsx';
 
 const Navigation = (props) => {
   const {onMenuSwitch, onModalSwitch, menuDefault, modalDefault, onSearchSwitch, onSearchChange} = props;
@@ -28,57 +25,32 @@ const Navigation = (props) => {
     onSearchChange(``);
   };
 
-  const navigationItemsData = [
-    {
-      id: uuid(),
-      button: {
-        className: menuDefault ? `button` : `button button--active`,
-        type: `button`,
-        dataType: `menu`,
-        events: {
-          onClick: onMenuButtonClick
-        }
-      },
-      icon: {
-        className: `button`,
-        name: `menu`,
-        width: 19,
-        height: 16
-      },
-    },
-    {
-      id: uuid(),
-      button: {
-        className: modalDefault ? `button` : `button button--active`,
-        type: `button`,
-        dataType: `add`,
-        events: {
-          onClick: onAddButtonClick
-        }
-      },
-      icon: {
-        className: `button`,
-        name: `add`,
-        width: 16,
-        height: 16
-      },
-    }
-  ];
-
-  const navigationItems = navigationItemsData.map((item) => {
-    return (
-      <li key={item.id} className="navigation__item">
-        <Button buttonData={item.button}>
-          <Icon iconData={item.icon}/>
-        </Button>
-      </li>
-    );
-  });
+  const menuButtonClassName = menuDefault ? `button` : `button button--active`;
+  const modalButtonClassName = modalDefault ? `button` : `button button--active`;
 
   return (
     <nav className="navigation">
       <ul className="navigation__list">
-        {navigationItems}
+        <li className="navigation__item">
+          <button className={menuButtonClassName} type="button" onClick={onMenuButtonClick}>
+            <svg className="button__icon" width="19" height="16" viewBox="0 0 19 16" fill="none"
+              xmlns="http://www.w3.org/2000/svg">
+              <path fillRule="evenodd" clipRule="evenodd"
+                d="M0 1.33333C0 0.979711 0.142984 0.640573 0.397498 0.390525C0.652012 0.140476 0.997206 0 1.35714 0H17.6429C18.0028 0 18.348 0.140476 18.6025 0.390525C18.857 0.640573 19 0.979711 19 1.33333C19 1.68696 18.857 2.02609 18.6025 2.27614C18.348 2.52619 18.0028 2.66667 17.6429 2.66667H1.35714C0.997206 2.66667 0.652012 2.52619 0.397498 2.27614C0.142984 2.02609 0 1.68696 0 1.33333ZM0 8C0 7.64638 0.142984 7.30724 0.397498 7.05719C0.652012 6.80714 0.997206 6.66667 1.35714 6.66667H9.5C9.85994 6.66667 10.2051 6.80714 10.4596 7.05719C10.7142 7.30724 10.8571 7.64638 10.8571 8C10.8571 8.35362 10.7142 8.69276 10.4596 8.94281C10.2051 9.19286 9.85994 9.33333 9.5 9.33333H1.35714C0.997206 9.33333 0.652012 9.19286 0.397498 8.94281C0.142984 8.69276 0 8.35362 0 8ZM0 14.6667C0 14.313 0.142984 13.9739 0.397498 13.7239C0.652012 13.4738 0.997206 13.3333 1.35714 13.3333H17.6429C18.0028 13.3333 18.348 13.4738 18.6025 13.7239C18.857 13.9739 19 14.313 19 14.6667C19 15.0203 18.857 15.3594 18.6025 15.6095C18.348 15.8595 18.0028 16 17.6429 16H1.35714C0.997206 16 0.652012 15.8595 0.397498 15.6095C0.142984 15.3594 0 15.0203 0 14.6667Z"
+                fill="white" />
+            </svg>
+          </button>
+        </li>
+        <li className="navigation__item">
+          <button className={modalButtonClassName} type="button" data-type="add" onClick={onAddButtonClick}>
+            <svg className="button__icon" width="16" height="16" viewBox="0 0 16 16" fill="none"
+              xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M16 8C16 8.73733 15.936 9.33333 15.1987 9.33333H9.33333V15.1987C9.33333 15.9347 8.73733 16 8 16C7.26267 16 6.66667 15.9347 6.66667 15.1987V9.33333H0.801333C0.0653331 9.33333 0 8.73733 0 8C0 7.26267 0.0653331 6.66667 0.801333 6.66667H6.66667V0.801333C6.66667 0.0639998 7.26267 0 8 0C8.73733 0 9.33333 0.0639998 9.33333 0.801333V6.66667H15.1987C15.936 6.66667 16 7.26267 16 8Z"
+                fill="white" />
+            </svg>
+          </button>
+        </li>
       </ul>
     </nav>
   );
