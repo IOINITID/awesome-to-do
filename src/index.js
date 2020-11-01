cf6d340704f379582be1ef4cab4fce5189a8d4aa
IOINITID <50173108+IOINITID@users.noreply.github.com>
2020-11-01 12:17:50 +0300

    Revert "Updated index.js file"
    This reverts commit 5a927c438d76bee6ceefeede4a88b8502637deef.

--- src/index.js
+++ src/index.js
@@ -4,12 +4,4 @@ import store from './store/index.js';
 import {Provider} from 'react-redux';
 import App from './components/app/app.jsx';
 
-const Root = () => {
-  return (
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
-};
-
-ReactDOM.render(<Root />, document.querySelector(`.root`));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.querySelector(`.root`));
