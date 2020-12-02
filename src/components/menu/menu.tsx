33805e9dc7d372e9070a6f42a33e5fe62460b7fb
IOINITID <50173108+IOINITID@users.noreply.github.com>
2020-12-02 12:32:50 +0300

    Added interface for menu component and removed prop-types

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {onMenuSwitchAction, onFilterChangeAction, onModalSwitchAction} from '../../actions/index';
 import FixedIcon from '../../assets/images/fixed-icon.svg';
 import AddIcon from '../../assets/images/add-icon.svg';
 
-const Menu = (props) => {
+interface IItemsData {
+  done: boolean;
+  fixed: boolean;
+  id: string;
+  more: boolean;
+  title: boolean;
+}
+
+interface IMenu {
+  isMenuOpen: boolean;
+  onModalSwitch: () => void;
+  onMenuSwitch: () => void;
+  onFilterChange: (firstArg: string) => void;
+  itemsData: Array<IItemsData>
+}
+
+const Menu = (props: IMenu) => {
   const {itemsData, isMenuOpen, onModalSwitch, onMenuSwitch, onFilterChange} = props;
 
   const itemsAll = itemsData.length;
@@ -66,14 +81,6 @@ const Menu = (props) => {
       </ul>
     </section>
   );
-};
-
-Menu.propTypes = {
-  isMenuOpen: PropTypes.bool.isRequired,
-  onModalSwitch: PropTypes.func.isRequired,
-  onMenuSwitch: PropTypes.func.isRequired,
-  onFilterChange: PropTypes.func.isRequired,
-  itemsData: PropTypes.array.isRequired
 };
 
 const mapStateToProps = (state) => {
