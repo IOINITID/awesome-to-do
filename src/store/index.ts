edd5db6afdef979aa7820bf9efd5ee5b1ccb84ad
IOINITID <50173108+IOINITID@users.noreply.github.com>
2020-12-07 10:11:31 +0300

    Added redux devtools extension to store file

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,9 @@
 import {createStore} from 'redux';
 import reducer from '../reducers/index';
 
-const store = createStore(reducer);
+// const store = createStore(reducer);
+
+const reduxDevTools = (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__();
+const store = process.env.NODE_ENV !== `production` && typeof window === `object` ? createStore(reducer, reduxDevTools) : createStore(reducer);
 
 export default store;
