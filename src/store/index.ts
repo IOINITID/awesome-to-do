a6ca113d84e33d553f7e5d49d6b6968d82d39717
IOINITID <50173108+IOINITID@users.noreply.github.com>
2020-12-07 10:16:17 +0300

    Updated store file

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,6 @@
 import {createStore} from 'redux';
 import reducer from '../reducers/index';
 
-const reduxDevTools = (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__();
-const store = process.env.NODE_ENV !== `production` && typeof window === `object` ? createStore(reducer, reduxDevTools) : createStore(reducer);
+const store = createStore(reducer);
 
 export default store;
