6f643d7f452219d4fa8adcb8688dfa30b75c021e
IOINITID <50173108+IOINITID@users.noreply.github.com>
2021-04-05 21:55:03 +0300

    Added dispatch hook, added react-redux types and updated logo components

--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -1,29 +1,21 @@
-import React from 'react';
+import React, { memo } from 'react';
 import LogoIcon from '../../assets/images/logo-icon.svg';
-import { connect } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { onWelcomeSwitchAction } from '../../actions';
 
-const Logo = ({ onWelcomeSwitch }: { onWelcomeSwitch: (isWellcome) => void }) => {
+const Logo = () => {
+  const dispatch = useDispatch();
+
+  const logoClickHandler = (event) => {
+    event.preventDefault();
+    dispatch(onWelcomeSwitchAction(true));
+  };
+
   return (
-    // eslint-disable-next-line jsx-a11y/anchor-is-valid
-    <a
-      className="logo"
-      href="#"
-      aria-label="Логотип Awesome To-Do."
-      onClick={(event) => {
-        event.preventDefault();
-        onWelcomeSwitch(true);
-      }}
-    >
+    <a className="logo" href="#no_scroll" aria-label="Логотип Awesome To-Do." onClick={logoClickHandler}>
       <LogoIcon className="logo__icon" width="92" height="65" />
     </a>
   );
 };
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    onWelcomeSwitch: (isWellcome) => dispatch(onWelcomeSwitchAction(isWellcome)),
-  };
-};
-
-export default connect(null, mapDispatchToProps)(Logo);
+export default memo(Logo);
