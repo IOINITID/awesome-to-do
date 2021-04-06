04e8c581993e5224472f2cf0f50d7ccb2a9a7bf6
IOINITID <50173108+IOINITID@users.noreply.github.com>
2021-04-06 21:51:55 +0300

    Updated switch component to hooks

--- src/components/switch/switch.tsx
+++ src/components/switch/switch.tsx
@@ -1,23 +1,20 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-import React from 'react';
-import { connect } from 'react-redux';
+import React, { memo } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { onThemeSwitchAction } from '../../actions/index';
 import MoonIcon from '../../assets/images/moon-icon.svg';
 import SunIcon from '../../assets/images/sun-icon.svg';
 
-interface ISwitch {
-  theme: string;
-  onThemeSwitch: () => void;
-}
-
-const Switch = (props: ISwitch) => {
-  const { theme, onThemeSwitch } = props;
+const Switch = () => {
+  const dispatch = useDispatch();
+  const theme = useSelector<any>((state) => state.theme);
+  const switchClickHandler = () => dispatch(onThemeSwitchAction());
 
   return (
-    <div className="switch" onClick={onThemeSwitch}>
+    <div className="switch" onClick={switchClickHandler}>
       <button className="button switch__button" aria-label="Переключить тему.">
-        {theme === `dark` ? (
+        {theme === 'dark' ? (
           <MoonIcon className="button__icon button__icon--moon" width="16" height="16" />
         ) : (
           <SunIcon className="button__icon button__icon--sun" width="16" height="16" />
@@ -27,16 +24,4 @@ const Switch = (props: ISwitch) => {
   );
 };
 
-const mapStateToProps = (state) => {
-  return {
-    theme: state.theme,
-  };
-};
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    onThemeSwitch: () => dispatch(onThemeSwitchAction()),
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Switch);
+export default memo(Switch);
