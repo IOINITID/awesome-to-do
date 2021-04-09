f55a70a3f9576dca988a340492eea7c7303f412f
IOINITID <50173108+IOINITID@users.noreply.github.com>
2021-04-09 23:49:44 +0300

    Removed unused actions

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -5,68 +5,10 @@ interface IAction {
   payload?: any;
 }
 
-const onLanguageChangeAction = () => {
-  return {
-    type: 'LANGUAGE_CHANGE',
-  };
-};
-
-const onThemeSwitchAction = (): IAction => {
-  return {
-    type: ActionTypes.THEME_SWITCH,
-  };
-};
-
-const onMenuSwitchAction = (): IAction => {
-  return {
-    type: ActionTypes.MENU_SWITCH,
-  };
-};
-
-const onSearchSwitchAction = (): IAction => {
-  return {
-    type: ActionTypes.SEARCH_SWITCH,
-  };
-};
-
-const onSearchCloseAction = (): IAction => {
-  return {
-    type: ActionTypes.SEARCH_CLOSE,
-  };
-};
-
-const onSearchChangeAction = (searchData: string = ``): IAction => {
-  return {
-    type: ActionTypes.SEARCH_CHANGE,
-    payload: searchData,
-  };
-};
-
-const onSearchingAction = (searching: boolean): IAction => {
-  return {
-    type: ActionTypes.SEARCHING,
-    payload: searching,
-  };
-};
-
-const onWelcomeSwitchAction = (isWelcome = false): IAction => {
-  return {
-    type: ActionTypes.WELCOME_SWITCH,
-    payload: isWelcome,
-  };
-};
-
 const onTaskAddAction = (title: string): IAction => {
   return {
     type: ActionTypes.TASK_ADD,
     payload: title,
-  };
-};
-
-const onFilterChangeAction = (filterType: string): IAction => {
-  return {
-    type: ActionTypes.FILTER_CHANGE,
-    payload: filterType,
   };
 };
 
@@ -125,21 +67,12 @@ const onMoreCloseAction = (): IAction => {
 };
 
 export {
-  onThemeSwitchAction,
-  onMenuSwitchAction,
-  onSearchSwitchAction,
-  onSearchChangeAction,
-  onSearchingAction,
-  onWelcomeSwitchAction,
   onTaskAddAction,
-  onFilterChangeAction,
   onTaskDeleteAction,
   onTaskFixedAction,
   onTaskDoneAction,
   onTaskEditAction,
   onModalSwitchAction,
   onMoreSwitchAction,
-  onSearchCloseAction,
   onMoreCloseAction,
-  onLanguageChangeAction,
 };
