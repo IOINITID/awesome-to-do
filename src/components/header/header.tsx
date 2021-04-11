e5f66b15689ac0dcb8e87b6feae2388bc9c4b20a
IOINITID <50173108+IOINITID@users.noreply.github.com>
2021-04-11 16:50:22 +0300

    Updated switch component styles

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import Navigation from '../navigation/navigation';
 import Search from '../search/search';
-import Switch from '../switch/switch';
+import Switch from '../switch';
 import Logo from '../logo/logo';
+import MoonIcon from '../../assets/images/moon-icon.svg';
+import SunIcon from '../../assets/images/sun-icon.svg';
+import { useDispatchTyped, useSelectorTyped } from '../../hooks';
+import { selectTheme, themeSwitch } from '../../features/theme/themeSlice';
 
 const Header = () => {
+  const dispatch = useDispatchTyped();
+  const theme = useSelectorTyped(selectTheme);
+  const switchClickHandler = () => dispatch(themeSwitch());
+
   return (
     <header className="header">
       <div className="container container--flex">
         <Navigation />
         <Search />
-        <Switch />
+        <Switch active={theme === 'light'} onClick={switchClickHandler}>
+          {theme === 'light' ? <MoonIcon /> : <SunIcon />}
+        </Switch>
         <Logo />
       </div>
     </header>
--- src/components/switch/switch.tsx
+++ src/components/switch/switch.tsx
@@ -1,25 +1,13 @@
-/* eslint-disable jsx-a11y/no-static-element-interactions */
-/* eslint-disable jsx-a11y/click-events-have-key-events */
-import React, { memo } from 'react';
-import MoonIcon from '../../assets/images/moon-icon.svg';
-import SunIcon from '../../assets/images/sun-icon.svg';
-import { useSelectorTyped, useDispatchTyped } from '../../hooks';
-import { selectTheme, themeSwitch } from '../../features/theme/themeSlice';
+import React, { ReactElement } from 'react';
 import Button from '../button';
 import { StyledSwitch } from './styled';
 
-const Switch = () => {
-  const dispatch = useDispatchTyped();
-  const theme = useSelectorTyped(selectTheme);
-  const switchClickHandler = () => dispatch(themeSwitch());
-
+const Switch = ({ children, ...props }: { children?: ReactElement; active: boolean; onClick?: () => void }) => {
   return (
-    // <div className="switch" onClick={switchClickHandler}>
-    <StyledSwitch active={theme === 'light'} onClick={switchClickHandler}>
-      <Button aria-label="Переключить тему.">{theme === 'dark' ? <MoonIcon /> : <SunIcon />}</Button>
+    <StyledSwitch {...props}>
+      <Button>{children}</Button>
     </StyledSwitch>
-    // </div>
   );
 };
 
-export default memo(Switch);
+export default Switch;
