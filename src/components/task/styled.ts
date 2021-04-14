e1dbc392abac2db9bf9ea725dbe63f42403d31cf
IOINITID <50173108+IOINITID@users.noreply.github.com>
2021-04-14 20:57:05 +0300

    Updated task component

---
+++ src/components/task/styled.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const StyledTaskButtons = styled.div`
+  display: grid;
+  grid-auto-flow: column;
+  column-gap: 30px;
+`;
--- src/components/task/task.tsx
+++ src/components/task/task.tsx
@@ -1,11 +1,12 @@
 import React, { Fragment, memo } from 'react';
 import More from '../more/more';
-import { useDispatch } from 'react-redux';
 import DoneIcon from '../../assets/images/done-icon.svg';
 import MoreIcon from '../../assets/images/more-icon.svg';
 import { doneTask, taskMoreSwitch } from '../../features/tasks/tasksSlice';
 import Button from '../button';
 import { StyledTasksDescription } from '../tasks/styled';
+import { StyledTaskButtons } from './styled';
+import { useDispatchTyped } from '../../hooks';
 
 interface ITask {
   id: string;
@@ -16,7 +17,7 @@ interface ITask {
 }
 
 const Task = ({ id, value, done, fixed, more }: ITask) => {
-  const dispatch = useDispatch();
+  const dispatch = useDispatchTyped();
 
   const moreButtonClickHandler = (): void => {
     dispatch(taskMoreSwitch(id));
@@ -29,23 +30,16 @@ const Task = ({ id, value, done, fixed, more }: ITask) => {
   return (
     <Fragment>
       <StyledTasksDescription>{value}</StyledTasksDescription>
-      <div
-        style={{
-          display: 'grid',
-          gridAutoFlow: 'column',
-          columnGap: '30px',
-        }}
-      >
+      <StyledTaskButtons>
         {!done && (
           <Button outline onClick={doneButtonClickHandler}>
             <DoneIcon />
           </Button>
         )}
-
         <Button active={more} outline onClick={moreButtonClickHandler}>
           <MoreIcon />
         </Button>
-      </div>
+      </StyledTaskButtons>
       <More id={id} done={done} fixed={fixed} more={more} />
     </Fragment>
   );
